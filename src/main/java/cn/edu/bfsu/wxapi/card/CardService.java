db285e2adc3d81b08f52dbaab55e009e14027b0e
stenio <stenio@163.com>
2015-09-01 11:11:55 +0800

    modify api docs

--- src/main/java/cn/edu/bfsu/wxapi/card/CardService.java
+++ src/main/java/cn/edu/bfsu/wxapi/card/CardService.java
@@ -19,7 +19,9 @@ public interface CardService {
 	 * 
 	 * @param id
 	 *            用户ID
-	 * @return 当前余额
+	 * @return "id" 用户ID
+	 * 		   "name" 用户姓名
+	 * 		   "balance" 余额
 	 */
 	public Map<String, String> getBalance(String id);
 
