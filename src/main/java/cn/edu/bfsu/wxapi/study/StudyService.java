0c68d684d71616e288e56daa443e66d56a271ec1
stenio <stenio@163.com>
2015-10-10 21:59:29 +0800

    修改课程表接口

--- src/main/java/cn/edu/bfsu/wxapi/study/StudyService.java
+++ src/main/java/cn/edu/bfsu/wxapi/study/StudyService.java
@@ -14,5 +14,14 @@ public interface StudyService {
 	 */
 	List<ScoreInfo> listScore(String id, String year, String term);
 
-	List<CourseInfo> listCourse(String id, String year, String term);
+	/**
+	 * 获取成绩列表
+	 * 
+	 * @param id 学号
+	 * @param year 年份
+	 * @param term 学期（春、秋）
+	 * @param date 日期
+	 * @return
+	 */
+	List<CourseInfo> listCourse(String id, String year, String term, String date);
 }
